11569233eccafa8cf2200919aea4f45228b85d6e
Hachim Kidjei <hachimkidjei@gmail.com>
2025-05-23 01:27:15 +0200

    Initial commit

---
+++ src/main/java/org/miage/procrastinapp/ProcrastinappApplication.java
@@ -0,0 +1,13 @@
+package org.miage.procrastinapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcrastinappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProcrastinappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/miage/procrastinapp/ProcrastinappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miage.procrastinapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcrastinappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
